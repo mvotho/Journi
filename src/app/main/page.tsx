2056159ebcd652d869fe0a2f9c07aca05d702b48
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-01-27 21:58:03 +0000

    Added daisyui for protoyping, added basic layout

---
+++ src/app/main/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold">
+        test
+      </h1>
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import { ArrowRightIcon } from '@heroicons/react/24/solid'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
+      <div className="flex justify-center items-center">
+        <div className='grid grid-cols-2 gap-2'>
+          <div className='flex flex-row justify-center items-center'>
+            <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
+            <div className='flex justify-center items-center'>
+              <ArrowRightIcon className="h-6 w-6 text-black-500" />
+            </div>
+          </div>
+        <div>WYSIWYG</div>
+        </div>
+      </div>
     </main>
   )
 }
