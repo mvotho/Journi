f42532d65b0798305f0042af7296174acbbc13a3
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-01-29 23:52:28 +0000

    Added basic text editor lexical

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { Inter } from '@next/font/google'
 import { ArrowRightIcon } from '@heroicons/react/24/solid'
+import Editor from "../components/Editor"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center pt-8">
         <div className='grid grid-cols-2 gap-2'>
           <div className='flex flex-row justify-center items-center'>
             <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
@@ -14,7 +15,7 @@ export default function Home() {
               <ArrowRightIcon className="h-6 w-6 text-black-500" />
             </div>
           </div>
-        <div>WYSIWYG</div>
+        <div><Editor></Editor></div>
         </div>
       </div>
     </main>
