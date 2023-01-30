2b860556d8639ece3db2c41acd37822fab3119d7
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-01-30 21:28:09 +0000

    updated styling and fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,17 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main>
-      <div className="flex justify-center items-center pt-8">
-        <div className='grid grid-cols-2 gap-2'>
-          <div className='flex flex-row justify-center items-center'>
+      <div className="flex justify-center items-center pt-24">
+        <div className='grid grid-cols-2 gap-8'>
+          <div className='flex flex-row justify-center pr-8'>
             <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
-            <div className='flex justify-center items-center'>
+            <div className='flex justify-center mt-3 ml-1'>
               <ArrowRightIcon className="h-6 w-6 text-black-500" />
             </div>
           </div>
-        <div><Editor></Editor></div>
+          <div>
+            <Editor></Editor>
+          </div>
         </div>
       </div>
     </main>
