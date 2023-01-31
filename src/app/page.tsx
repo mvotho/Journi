441d0cebf7943d513491b0fdeb06b6b162ae87e8
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-01-31 20:42:40 +0000

    pocketbase setup and basic get of entries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import { Inter } from '@next/font/google'
 import { ArrowRightIcon } from '@heroicons/react/24/solid'
 import Editor from "../components/Editor"
+import PocketBase from 'pocketbase';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+
+async function getPosts() {
+  const res = await fetch("http://127.0.0.1:8090/api/collections/posts/records")
+  const data = await res.json();
+  return data?.items as any[];
+}
+
+
+export default async function Home() {
+    const records = await getPosts();
+  
+
   return (
     <main>
       <div className="flex justify-center items-center pt-24">
@@ -14,6 +26,9 @@ export default function Home() {
             <div className='flex justify-center mt-3 ml-1'>
               <ArrowRightIcon className="h-6 w-6 text-black-500" />
             </div>
+            <div>{records.map((entry)=>{
+            return <div>{entry.preview}</div>}
+            )}</div>
           </div>
           <div>
             <Editor></Editor>
